1b58602624a440f2d1abaa63b7eb753acc3a3682
jefersonsalazar2000-gif <jefersonsalazar2000@gmail.com>
2026-06-28 17:59:14 -0400

    Update import for createClient from libsql client

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path       = require('path');
 const bcrypt     = require('bcryptjs');
 const jwt        = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
-const { createClient } = require('@libsql/client');
+const { createClient } = require('@libsql/client/http');
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
